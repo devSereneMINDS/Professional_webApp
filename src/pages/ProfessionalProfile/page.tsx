777c9e2675fbf2645705c50bbe237c5d059291d0
kshitiz-monkecoder <kshitizbhardwaj100@gmail.com>
2025-05-20 16:25:25 +0530

    resolved 2

--- src/pages/ProfessionalProfile/page.tsx
+++ src/pages/ProfessionalProfile/page.tsx
@@ -14,72 +14,16 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import ProfileCard from './components/ProfileCard';
 import dayjs from 'dayjs';
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 
-interface ClientQnA {
-  gender: string;
-  'age-group': string;
-  occupation: string;
-  'marital-status': string;
-}
 
-interface ClientData {
-  id: number;
-  name: string;
-  age: number | null;
-  email: string;
-  sex: string | null;
-  phone_no: string;
-  diagnosis: string | null;
-  photo_url: string;
-  zipcode: string | null;
-  city: string | null;
-  appointment_id: number | null;
-  assessment_id: number | null;
-  invoice_id: number | null;
-  medical_record_id: number | null;
-  created_at: string;
-  updated_at: string;
-  q_and_a: ClientQnA;
-  uid: string;
-  no_of_sessions: number;
-}
 
 export default function JoyOrderDashboardTemplate() {
   const { id } = useParams<{ id?: string }>(); 
   //const navigate = useNavigate();
-  const [clientData, setClientData] = useState<ClientData | null>(null);
-  const [, setLoading] = useState<boolean>(false);
-  const [, setError] = useState<string | null>(null);
 
   console.log("Client ID",id)
 
   // Fetch client data when clientId changes
-  useEffect(() => {
-    if (id) {
-      const fetchClientData = async () => {
-        try {
-          setLoading(true);
-          const response = await fetch(`https://api.sereneminds.life/api/clients2/${id}`);
-          if (!response.ok) {
-            throw new Error('Failed to fetch client data');
-          }
-          const data: ClientData = await response.json();
-          setClientData(data);
-        } catch (err) {
-          setError(err instanceof Error ? err.message : 'Unknown error');
-          console.error('Error fetching client data:', err);
-        } finally {
-          setLoading(false);
-        }
-      };
-      fetchClientData();
-    } else {
-      setClientData(null); // Clear data if no clientId
-      setError(null);
-      setLoading(false);
-    }
-  }, [id]);
   
   return (
     <CssVarsProvider disableTransitionOnChange>
@@ -181,10 +125,9 @@ export default function JoyOrderDashboardTemplate() {
                 width:"100%",
                 gap: 2,
                 mt: 1,
-                width: '100%',
               }}
             >
-              <ProfileCard clientData={clientData}/>
+              <ProfileCard/>
             </Box>
 
           </Box>
